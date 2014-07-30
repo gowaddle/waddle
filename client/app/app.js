dd5f363a90128f886c0857ea99d3ecb1fa7f5466
Vishal Reddy <vreddy18@gmail.com>
2014-07-30 18:55:59 -0400

    Edit gruntfile to include browserify

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,23 @@
+var angular = require('angular');
+angular.module('app', [])
+
+.controller('Ctrl', function ($scope) {
+	$scope.data = {name: 'yooooo'};
+})
+
+.directive('myThing', function () {
+	return {
+		restrict: 'E',
+		template: '<div><input><button ng-click="change()">change</button></div>',
+		scope: {
+			things: '='
+		},
+		link: function (scope, ele, attr) {
+			scope.change = function () {
+				scope.things.name = 'something';
+			};
+		}
+	};
+});
+
 
