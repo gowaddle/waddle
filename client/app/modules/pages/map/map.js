cda43c5357703e3d924aa0a586f647cd1ed16c9e
Vishal Reddy <vreddy18@gmail.com>
2014-08-01 00:35:43 -0400

    Render facebook checkins as map markers

--- client/app/modules/pages/map/map.js
+++ client/app/modules/pages/map/map.js
@@ -7,9 +7,31 @@ angular.module('waddle.map', [])
 	      attributionControl: false,
 	      zoomControl: false
 	    }).setView([37.6, -122.45], 3);
+  	$scope.facebookPlaces = L.layerGroup().addTo($scope.map);
+  	$scope.getMapData = function() {
+		  FacebookMapData.getFacebookMapData().then(function(data){
+		  	console.log("we got data:", data)
+		  	var fbData = data.data;
+		  	for(var i = 0; i < fbData.length; i++) {
+		  		var checkin = fbData[i].place;
+		  		var checkinLatLng = L.latLng(checkin.location.latitude, checkin.location.longitude);
+		  		$scope.makeMarker(checkin.name, checkinLatLng)
+		  	}
+		  })
+  	};
+  	$scope.makeMarker = function(placeName, latLng) {
+        L.marker(latLng, {
+                        icon: L.mapbox.marker.icon({
+                            'marker-color': '1087bf',
+                            'marker-size': 'large',
+                            'marker-symbol': 'circle-stroked'
+                        })
+                    })
+                   .bindPopup('<h2>' + placeName + '</h2>')
+                   .addTo($scope.facebookPlaces);
+    };
+    $scope.getMapData();
 	  });
+  });
 
-	  FacebookMapData.getFacebookMapData().then(function(data){
-	  	console.log("we got data:", data)
-	  })
-  })
+
