36188b8e6c604325716c24ce8e1ff86dcd56867f
Ajay Tungare <ajay.tungare@gmail.com>
2014-07-31 20:34:26 -0700

    Implement Logout button

--- client/app/modules/services/auth.js
+++ client/app/modules/services/auth.js
@@ -1,25 +1,33 @@
 angular.module('waddle.services.auth', [])
   .factory('Auth', function ($q, $state) {
 
-    var checkLogin = function () {
-      var deferred = $q.defer();
+  var checkLogin = function () {
+    var deferred = $q.defer();
 
-      openFB.getLoginStatus(function(response){
-        if (response.status === 'connected'){
-          console.log('connected');
-          deferred.resolve();
-        } else {
-          console.log('not connected');
-          $state.go('frontpage');
-          deferred.reject('not connected');
-        }
-      });
+    openFB.getLoginStatus(function(response){
+      if (response.status === 'connected'){
+        console.log('connected');
+        deferred.resolve();
+      } else {
+        console.log('not connected');
+        $state.go('frontpage');
+        deferred.reject('not connected');
+      }
+    });
 
-      return deferred.promise;
-    };
+    return deferred.promise;
+  };
 
-    return {
-      checkLogin: checkLogin
-    }
+  var logout = function () {
+    console.log('logout');
+    openFB.logout(function() {
+      $state.go('frontpage');
+    });
+  };
+
+  return {
+    checkLogin: checkLogin,
+    logout: logout
+  }
     
   });
