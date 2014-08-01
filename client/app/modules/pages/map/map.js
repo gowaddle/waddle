9919b404a9af7d8cde08ddade34a2099a195b2bd
Vishal Reddy <vreddy18@gmail.com>
2014-08-01 00:51:14 -0400

    Clean up code in map.js

--- client/app/modules/pages/map/map.js
+++ client/app/modules/pages/map/map.js
@@ -1,37 +1,43 @@
 angular.module('waddle.map', [])
-
   .controller('MapController', function ($scope, $state, Auth, FacebookMapData) {
+
     $scope.logout = Auth.logout;
+
   	Auth.checkLogin().then(function(){
 	  	$scope.map = L.mapbox.map('map', 'injeyeo2.i9nn801b', {
 	      attributionControl: false,
 	      zoomControl: false
 	    }).setView([37.6, -122.45], 3);
-  	$scope.facebookPlaces = L.layerGroup().addTo($scope.map);
-  	$scope.getMapData = function() {
-		  FacebookMapData.getFacebookMapData().then(function(data){
-		  	console.log("we got data:", data)
-		  	var fbData = data.data;
-		  	for(var i = 0; i < fbData.length; i++) {
-		  		var checkin = fbData[i].place;
-		  		var checkinLatLng = L.latLng(checkin.location.latitude, checkin.location.longitude);
-		  		$scope.makeMarker(checkin.name, checkinLatLng)
-		  	}
-		  })
-  	};
-  	$scope.makeMarker = function(placeName, latLng) {
-        L.marker(latLng, {
-                        icon: L.mapbox.marker.icon({
-                            'marker-color': '1087bf',
-                            'marker-size': 'large',
-                            'marker-symbol': 'circle-stroked'
-                        })
-                    })
-                   .bindPopup('<h2>' + placeName + '</h2>')
-                   .addTo($scope.facebookPlaces);
-    };
-    $scope.getMapData();
+
+	  	$scope.facebookPlaces = L.layerGroup().addTo($scope.map);
+	  	
+	   	$scope.getMapData = function() {
+			  FacebookMapData.getFacebookMapData().then(function(data){
+			  	console.log("we got data:", data)
+			  	var fbData = data.data;
+			  	for(var i = 0; i < fbData.length; i++) {
+			  		var checkin = fbData[i].place;
+			  		var checkinLatLng = L.latLng(checkin.location.latitude, checkin.location.longitude);
+			  		$scope.makeMarker(checkin.name, checkinLatLng)
+			  	}
+			  })
+	  	};
+
+	    $scope.getMapData();
 	  });
+
+
+  	$scope.makeMarker = function(placeName, latLng) {
+      L.marker(latLng, {
+	      icon: L.mapbox.marker.icon({
+          'marker-color': '1087bf',
+          'marker-size': 'large',
+          'marker-symbol': 'circle-stroked'
+        })
+	    })
+	    .bindPopup('<h2>' + placeName + '</h2>')
+	    .addTo($scope.facebookPlaces);
+    };
   });
 
 
