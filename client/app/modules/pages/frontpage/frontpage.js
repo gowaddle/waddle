cb15692970662ad8a5d141c5dfb04f8f54e3268e
Michael Barton <mbarton.mail@gmail.com>
2014-07-31 19:01:57 -0700

    Add basic login function to frontpage controller

--- client/app/modules/pages/frontpage/frontpage.js
+++ client/app/modules/pages/frontpage/frontpage.js
@@ -1,1 +1,19 @@
-angular.module('waddle.frontpage', []);
+angular.module('waddle.frontpage', [])
+
+.controller('FrontpageController', function($scope, $state) {
+
+  openFB.getLoginStatus(function(response){
+  	if (response.status === 'connected'){
+  	  console.log('connected')
+  	} else {
+  	  $scope.login = function(){
+  	  	openFB.login(function(response){
+  	  	  if (response.status === 'connected'){
+  	  	  	console.log('connected')
+  	  	  }
+  	  	}, {scope: 'user_friends'});
+  	  }
+  	}
+  })
+
+});
