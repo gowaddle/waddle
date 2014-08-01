12c2b63c51b7155376891adae689247bcd47c727
Michael Barton <mbarton.mail@gmail.com>
2014-07-31 19:13:35 -0700

    Add local storage to login

--- client/app/modules/pages/frontpage/frontpage.js
+++ client/app/modules/pages/frontpage/frontpage.js
@@ -1,16 +1,23 @@
 angular.module('waddle.frontpage', [])
 
-.controller('FrontpageController', function($scope, $state) {
+.controller('FrontpageController', function($scope, $state, $window) {
 
   openFB.getLoginStatus(function(response){
   	if (response.status === 'connected'){
   	  console.log('connected')
   	} else {
+  	  console.log('not connected')
   	  $scope.login = function(){
   	  	openFB.login(function(response){
-  	  	  if (response.status === 'connected'){
-  	  	  	console.log('connected')
-  	  	  }
+          if(response.status === 'connected') {
+            openFB.api({path: '/me', success: function(data){
+              window.localStorage.setItem('FBuserID', data.id);
+              window.localStorage.setItem('FBuserName', data.name);
+              window.localStorage.setItem('FBuserLocale', data.locale);
+            }, error: function(err) {console.log(err);}});
+          } else {
+            alert('Facebook login failed: ' + response.error);
+          }
   	  	}, {scope: 'user_friends'});
   	  }
   	}
