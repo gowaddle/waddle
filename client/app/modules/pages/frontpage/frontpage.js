1db7d31fa3f4cbb20b7d7234657b743802aa9d51
Ajay Tungare <ajay.tungare@gmail.com>
2014-08-01 10:59:57 -0700

    Fix to redirect to map on login

--- client/app/modules/pages/frontpage/frontpage.js
+++ client/app/modules/pages/frontpage/frontpage.js
@@ -12,10 +12,11 @@ angular.module('waddle.frontpage', [])
   	  	openFB.login(function(response){
           if(response.status === 'connected') {
             openFB.api({path: '/me', success: function(data){
-              console.log(data)
+              console.log("data: ", data)
               window.localStorage.setItem('FBuserID', data.id);
               window.localStorage.setItem('FBuserName', data.name);
               window.localStorage.setItem('FBuserLocale', data.locale);
+              $state.go('map');
             }, error: function(err) {console.log(err);}});
           } else {
             alert('Facebook login failed: ' + response.error);
