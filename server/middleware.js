d6cb869766358d76dabc1f898e9c3a51e15aa365
Michael Barton <mbarton.mail@gmail.com>
2014-08-01 15:03:20 -0700

    Add scaffold for user routes, remove linting

--- server/middleware.js
+++ server/middleware.js
@@ -3,9 +3,14 @@ var morgan = require('morgan');
 var path = require('path');
 
 module.exports = function (app, express) {
+	var userRouter = express.Router();
+
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
 	app.use(express.static(path.join(__dirname, '../client')));
+
+	app.use('api/users', userRouter);
+	require('./api/users/userRoutes.js')(userRouter);
 
 };
