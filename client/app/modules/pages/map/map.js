6c42b91526cd0c051b0ad04d577004f4e173f122
Vishal Reddy <vreddy18@gmail.com>
2014-08-05 14:48:19 -0400

    Fix marker clustering

--- client/app/modules/pages/map/map.js
+++ client/app/modules/pages/map/map.js
@@ -13,9 +13,9 @@ angular.module('waddle.map', [])
         worldCopyJump: true
       }).setView([37.6, -122.45], 3);
 
-    	var facebookPlaces = L.layerGroup().addTo(configuredMap);
-    	var shadedCountries = L.mapbox.featureLayer().addTo(configuredMap);
+      var shadedCountries = L.mapbox.featureLayer().addTo(configuredMap);
       var aggregatedMarkers = new L.MarkerClusterGroup();
+    	// var facebookPlaces = L.layerGroup().addTo(configuredMap);
       configuredMap.addLayer(aggregatedMarkers);
     
     	var makeMarker = function (placeName, latLng) {
@@ -27,7 +27,7 @@ angular.module('waddle.map', [])
           })
   	    })
   	    .bindPopup('<h2>' + placeName + '</h2>')
-  	    .addTo(facebookPlaces);
+  	    // .addTo(facebookPlaces);
 
         aggregatedMarkers.addLayer(marker);
       };
@@ -48,47 +48,47 @@ angular.module('waddle.map', [])
        return deferred.promise;
       };
 
-    	$scope.countriesBeen = ["United States", "China"];
+    	// $scope.countriesBeen = ["United States", "China"];
 
-      var findCountriesBeen = function (fbData) {
-      	var deferred = $q.defer();
-      	deferredPromises = [];
-      	$scope.countriesBeen = [];
-      	for(var j = 0; j < fbData.length; j++) {
-	      	var deferred = $q.defer();
-      		var checkin = fbData[j].place;
-	      	Geocoder.reverseGeocode(checkin.location.longitude, checkin.location.latitude)
-            .then(function(data) {
-            	var countryName = data.data.features[0].place_name;
-	          	if($scope.countriesBeen.indexOf(countryName) === -1) {
-	          		$scope.countriesBeen.push(countryName);
-	          	}
-	          	deferred.resolve($scope.countriesBeen);
-            })
-          .then(function(data) {
-	      	  deferred.resolve($scope.countriesBeen);
-	      	  return deferred.promise;
-          })
-      	}
-      };
+     //  var findCountriesBeen = function (fbData) {
+     //  	var deferred = $q.defer();
+     //  	deferredPromises = [];
+     //  	$scope.countriesBeen = [];
+     //  	for(var j = 0; j < fbData.length; j++) {
+	    //   	var deferred = $q.defer();
+     //  		var checkin = fbData[j].place;
+	    //   	Geocoder.reverseGeocode(checkin.location.longitude, checkin.location.latitude)
+     //        .then(function(data) {
+     //        	var countryName = data.data.features[0].place_name;
+	    //       	if($scope.countriesBeen.indexOf(countryName) === -1) {
+	    //       		$scope.countriesBeen.push(countryName);
+	    //       	}
+	    //       	deferred.resolve($scope.countriesBeen);
+     //        })
+     //      .then(function(data) {
+	    //   	  deferred.resolve($scope.countriesBeen);
+	    //   	  return deferred.promise;
+     //      })
+     //  	}
+     //  };
 
-      var addToShadedCountries = function () {
-	      for(var j = 0; j < $scope.countriesBeen.length; j++) {
-	        for(var i = 0; i < globalCountryData.features.length; i++){
-	        var boundaries = globalCountryData.features[i].geometry.coordinates;
-		        if(globalCountryData.features[i].properties['NAME'] == $scope.countriesBeen[j]) {
-		          if(globalCountryData.features[i].geometry.type == 'MultiPolygon') {
-                console.log('hi');
-		            L.multiPolygon(boundaries, {stroke: false, opacity: 0.7, weight: 10, color:'#000', fillColor: '#000', fillOpacity: 0.7}).addTo(shadedCountries);
-		          }
-		          else {
-		            L.polygon(boundaries, {stroke: false, fillColor: '#000'}).addTo(shadedCountries);
-		          }
-		        }
-	        }
-	      }
-      }
-      addToShadedCountries();
+      // var addToShadedCountries = function () {
+	     //  for(var j = 0; j < $scope.countriesBeen.length; j++) {
+	     //    for(var i = 0; i < globalCountryData.features.length; i++){
+	     //    var boundaries = globalCountryData.features[i].geometry.coordinates;
+		    //     if(globalCountryData.features[i].properties['NAME'] == $scope.countriesBeen[j]) {
+		    //       if(globalCountryData.features[i].geometry.type == 'MultiPolygon') {
+      //           console.log('hi');
+		    //         L.multiPolygon(boundaries, {stroke: false, opacity: 0.7, weight: 10, color:'#000', fillColor: '#000', fillOpacity: 0.7}).addTo(shadedCountries);
+		    //       }
+		    //       else {
+		    //         L.polygon(boundaries, {stroke: false, fillColor: '#000'}).addTo(shadedCountries);
+		    //       }
+		    //     }
+	     //    }
+	     //  }
+      // }
+      // addToShadedCountries();
 
    
 
