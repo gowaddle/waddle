c2c073043a5a94dc5711ad6c3afa9f7a79665e80
Vishal Reddy <vreddy18@gmail.com>
2014-08-08 15:07:55 -0400

    Modify middleware to correctly add checkinRouter

--- server/middleware.js
+++ server/middleware.js
@@ -16,5 +16,19 @@ module.exports = function (app, express) {
   app.use(errorhandlers.errorHandler);
 
 	require('./api/users/userRoutes.js')(userRouter);
+};
+
+module.exports = function (app, express) {
+	var checkinRouter = express.Router();
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, '../client')));
+
+  app.use('/api/checkins', checkinRouter);
+  app.use(errorhandlers.errorLogger);
+  app.use(errorhandlers.errorHandler);
+
 	require('./api/checkins/checkinRoutes.js')(checkinRouter);
 };
