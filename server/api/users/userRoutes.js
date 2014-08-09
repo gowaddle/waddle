cbbf557ef01bd4ca5bc0787bced4419fef2c8b9e
Ajay Tungare <ajay.tungare@gmail.com>
2014-08-08 20:39:04 -0700

    complete sequence of requests from login click on client through data returned

--- server/api/users/userRoutes.js
+++ server/api/users/userRoutes.js
@@ -1,7 +1,7 @@
 var userController = require('./userController.js');
 
 module.exports = function(app){
-  app.post('/userdata', userController.updateUser);
+  app.post('/userdata', userController.userLogin);
   app.post('/userfoursquarecode', userController.addFoursquareData);
 /*	app.get('/:user', userController.getUserData);*/
 };
