2c9eb97d196375018e5ca19103333036447447e6
Michael Barton <mbarton.mail@gmail.com>
2014-08-11 19:00:13 -0700

    Add client side bucketlist framework

--- client/app/modules/pages/map/friends.js
+++ client/app/modules/pages/map/friends.js
@@ -1,11 +1,16 @@
 angular.module('waddle.friends', [])
   .controller('FriendsController', function ($scope, $state, UserRequests) {
+    var data = {
+      facebookID: window.sessionStorage.userFbID,
+      checkinID: $scope.data.currentCheckins[0]
+    }
+    //UserRequests.addToBucketList(data)
 
-      $scope.clickFriend = function (friend){
-        UserRequests.getUserData(friend)
-        .then(function(data){
-          console.log(data)
-          $scope.handleUserCheckinData(data.data)
-        });
-      }
+    $scope.clickFriend = function (friend){
+      UserRequests.getUserData(friend)
+      .then(function(data){
+        console.log(data)
+        $scope.handleUserCheckinData(data.data)
+      });
+    }
   });
