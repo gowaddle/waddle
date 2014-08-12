43c74ecb164d0f2aac678e8ad1f7ec84fcd3a996
Michael Barton <mbarton.mail@gmail.com>
2014-08-11 17:00:20 -0700

    Add server side route and function for adding checkin to bucket list

--- server/api/checkins/checkinRoutes.js
+++ server/api/checkins/checkinRoutes.js
@@ -2,4 +2,5 @@ var checkinController = require('./checkinController.js');
 
 module.exports = function (app){
   app.post('/realtimefsqdata', checkinController.realtimeFoursquareData);
+  app.post('/bucketlist', checkinController.addToBucketList);
 }
