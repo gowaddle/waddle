19a2bb9c69b8b2baec44be43cb223e85f0332531
Michael Barton <mbarton.mail@gmail.com>
2014-08-11 19:42:10 -0700

    Users can now add their friend's checkins to their bucket list

--- client/app/modules/pages/map/feed.js
+++ client/app/modules/pages/map/feed.js
@@ -1,10 +1,22 @@
 angular.module('waddle.feed', [])
+
   .controller('FeedController', function ($rootScope, $scope, UserRequests) {
-  console.log(UserRequests.allData)
+    console.log(UserRequests.allData)
   	$scope.allUserCheckinsFeed = {
-    get: function(index, count, success) {
-    	  var results = UserRequests.allData.data.allCheckins;
-        success(results);
+      get: function(index, count, success) {
+      	  //var results = UserRequests.allData.data.allCheckins;
+          var results = $scope.data.currentCheckins;
+          console.log(results)
+          success(results);
+      }
+    };
+
+    $scope.clickCheckin = function (checkin){
+      var data = {
+        facebookID: window.sessionStorage.userFbID,
+        checkinID: checkin
+      }
+       UserRequests.addToBucketList(data)
     }
-  };
+
   });
--- client/app/modules/pages/map/friends.js
+++ client/app/modules/pages/map/friends.js
@@ -1,16 +1,12 @@
 angular.module('waddle.friends', [])
   .controller('FriendsController', function ($scope, $state, UserRequests) {
-    var data = {
-      facebookID: window.sessionStorage.userFbID,
-      checkinID: $scope.data.currentCheckins[0].checkin.checkinID
-    }
-    UserRequests.addToBucketList(data)
-
+    
     $scope.clickFriend = function (friend){
       UserRequests.getUserData(friend)
       .then(function(data){
         console.log(data)
         $scope.handleUserCheckinData(data.data)
+        $state.go('map.feed')
       });
     }
   });
