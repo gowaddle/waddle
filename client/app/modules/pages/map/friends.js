bbdec447a497ecf7bf1511e0efda324e3bbeae9e
Michael Barton <mbarton.mail@gmail.com>
2014-08-13 13:19:04 -0700

    Refactor FriendsController to use best practices

--- client/app/modules/pages/map/friends.js
+++ client/app/modules/pages/map/friends.js
@@ -1,12 +1,15 @@
+FriendsController = function ($scope, $state, UserRequests){
+  $scope.clickFriend = function (friend){
+    UserRequests.getUserData(friend)
+    .then(function(data){
+      console.log(data)
+      $scope.handleUserCheckinData(data.data)
+      $state.go('map.feed')
+    });
+  };
+};
+
+FriendsController.$inject = ['$scope', '$state', 'UserRequests'];
+
 angular.module('waddle.friends', [])
-  .controller('FriendsController', function ($scope, $state, UserRequests) {
-    
-    $scope.clickFriend = function (friend){
-      UserRequests.getUserData(friend)
-      .then(function(data){
-        console.log(data)
-        $scope.handleUserCheckinData(data.data)
-        $state.go('map.feed')
-      });
-    }
-  });
+  .controller('FriendsController', FriendsController);
