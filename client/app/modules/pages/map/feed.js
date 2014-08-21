bdfad88e97a25cc595579b22c62d378475c90327
Michael Barton <mbarton.mail@gmail.com>
2014-08-21 08:52:57 -0700

    Refactor feed directive

--- client/app/modules/pages/map/feed.js
+++ client/app/modules/pages/map/feed.js
@@ -1,3 +1,4 @@
+// Controller for displaying feed which overlays map
 var FeedController = function (UserRequests, MapFactory, FootprintRequests, Auth, $rootScope, $scope, $state, $timeout, $stateParams){
   Auth.checkLogin()
   .then( function (){
@@ -51,73 +52,71 @@ var FeedController = function (UserRequests, MapFactory, FootprintRequests, Auth
   });
 }
 
+// Custom directive for submitting with ng-repeat on feed.
+// Avoids binding to multiple fields, especially if duplicates
+// Allows easy data manipulation in submit
+var customFeedSubmit = function(FootprintRequests){
+  return {
+      restrict: 'A',
+      link: function( scope , element , attributes ){
+        var $element = angular.element(element);
+        
+        // Add novalidate to the form element.
+        attributes.$set( 'novalidate' , 'novalidate' );
+        
+        $element.bind( 'submit' , function( e ) {
+          e.preventDefault();
+          
+          // Remove the class pristine from all form elements.
+          $element.find( '.ng-pristine' ).removeClass( 'ng-pristine' );
+          
+          // Get the form object.
+          var form = scope[ attributes.name ];
+          
+          // Set all the fields to dirty and apply the changes on the scope so that
+          // validation errors are shown on submit only.
+          angular.forEach( form , function( formElement , fieldName ) {
+              // If the fieldname starts with a '$' sign, it means it's an Angular
+              // property or function. Skip those items.
+              if ( fieldName[0] === '$' ) return;
+              
+              formElement.$pristine = false;
+              formElement.$dirty = true;
+          });
+          
+          // Do not continue if the form is invalid.
+          if ( form.$invalid ) {
+              // Focus on the first field that is invalid.
+              $element.find( '.ng-invalid' ).first().focus();
+              
+              return false;
+          }
+          
+          // From this point and below, we can assume that the form is valid.
+          scope.$eval( attributes.customSubmit );
+
+          // Text can be found with $element[0][0].value or scope.data.currentComment
+          //ID can be found with $element.context.dataset['customSubmit']
+          var commentData = {
+            clickerID: window.sessionStorage.userFbID,
+            checkinID: scope.footprint.checkin.checkinID,
+            text: scope.comment
+          }
+
+          FootprintRequests.addComment(commentData)
+          .then(function (data){
+            scope.data.currentComment = ''
+          })
+          scope.comment = ""
+          scope.$apply();
+      });
+    }
+}
+
 FeedController.$inject = ['UserRequests', 'MapFactory', 'FootprintRequests', 'Auth', '$rootScope', '$scope', '$state', '$timeout', '$stateParams'];
 
 
 //Start creating Angular module
 angular.module('waddle.feed', [])
   .controller('FeedController', FeedController)
-  .directive( 'customSubmit' , function(FootprintRequests){
-    return {
-        restrict: 'A',
-        link: function( scope , element , attributes ){
-          var $element = angular.element(element);
-          
-          // Add novalidate to the form element.
-          attributes.$set( 'novalidate' , 'novalidate' );
-          
-          $element.bind( 'submit' , function( e ) {
-            e.preventDefault();
-            
-            // Remove the class pristine from all form elements.
-            $element.find( '.ng-pristine' ).removeClass( 'ng-pristine' );
-            
-            // Get the form object.
-            var form = scope[ attributes.name ];
-            
-            // Set all the fields to dirty and apply the changes on the scope so that
-            // validation errors are shown on submit only.
-            angular.forEach( form , function( formElement , fieldName ) {
-                // If the fieldname starts with a '$' sign, it means it's an Angular
-                // property or function. Skip those items.
-                if ( fieldName[0] === '$' ) return;
-                
-                formElement.$pristine = false;
-                formElement.$dirty = true;
-            });
-            
-            // Do not continue if the form is invalid.
-            if ( form.$invalid ) {
-                // Focus on the first field that is invalid.
-                $element.find( '.ng-invalid' ).first().focus();
-                
-                return false;
-            }
-            
-            console.log("element")
-            console.log($element)               
-            console.log("scope")
-            console.log(scope)
-            // From this point and below, we can assume that the form is valid.
-            scope.$eval( attributes.customSubmit );
-
-            //Text can be found with $element[0][0].value or scope.data.currentComment
-            //ID can be found with $element.context.dataset['customSubmit']
-            var commentData = {
-              clickerID: window.sessionStorage.userFbID,
-              checkinID: scope.footprint.checkin.checkinID,
-              text: scope.comment
-            }
-
-            FootprintRequests.addComment(commentData)
-            .then(function (data){
-              scope.data.currentComment = ''
-              //$element[0][0].value = ''
-            })
-            console.log(commentData)
-            scope.comment = ""
-            scope.$apply();
-        });
-      }
-    };
-  });
+  .directive( 'customSubmit' , customFeedSubmit);
