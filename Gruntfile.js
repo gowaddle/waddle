3a405b99395b00c702ff387f19e6327634dcfeff
Michael Barton <mbarton.mail@gmail.com>
2014-08-22 18:58:34 -0700

    Remove bower install on grunt dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,7 +112,7 @@ module.exports = function (grunt) {
 		nodemon.stdout.pipe(process.stdout);
 		nodemon.stderr.pipe(process.stderr);
 
-		grunt.task.run(['clean', 'bower', 'concat:bower', 'concat:client', 'stylus', 'uglify:dev', 'watch']);
+		grunt.task.run(['clean', 'concat:bower', 'concat:client', 'stylus', 'uglify:dev', 'watch']);
 	});
 
 	grunt.registerTask('build', ['clean', 'bower', 'concat:bower', 'concat:client', 'stylus', 'uglify:build']);
