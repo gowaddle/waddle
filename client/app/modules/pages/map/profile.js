10ba4b89c054aa6a5c1795c24ae18c42fa4d7296
byeo630 <byeo630@gmail.com>
2014-10-31 18:28:05 -0400

    Add styling to the navbar, feed, and profile box.

--- client/app/modules/pages/map/profile.js
+++ client/app/modules/pages/map/profile.js
@@ -1,14 +1,20 @@
 (function(){
 
-var ProfileController = function ($scope, $state){
+var ProfileController = function ($scope, $state, UserRequests){
 
 $scope.username = 'Bob Owen';
 
+
+
 $scope.picture = ''
 
+if(UserRequests.allData) {
+	console.log(UserRequests.allData);
+	
+}
 }
 // Inject all the depependent services needed by the controller
-ProfileController.$inject = ['$scope', '$state'];
+ProfileController.$inject = ['$scope', '$state', 'UserRequests'];
 
 //Start creating Angular module
 angular.module('waddle.profile', [])
